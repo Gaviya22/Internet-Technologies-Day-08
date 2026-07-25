275b9394f6301c0d370ed21c7a0b17cc32eedb0b
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-25 10:06:56 +0530

    Implement customer management functions and update HTML structure

--- app.js
+++ app.js
@@ -13,18 +13,65 @@ console.log("Hii");
 
 
 
-let customer = {
-    name: "kamal",
-    age: 12,
-    isActive: true
-};
+// let customer = {
+//     name: "kamal",
+//     age: 12,
+//     isActive: true
+// };
 
-let stringCustomer = JSON.stringify(customer);
+// let stringCustomer = JSON.stringify(customer);
 
-localStorage.setItem("customer", stringCustomer);
+// localStorage.setItem("customer", stringCustomer);
 
-let retCustomer = localStorage.getItem("customer");
+// let retCustomer = localStorage.getItem("customer");
 
-let jsonCustomer = JSON.parse(retCustomer);
+// let jsonCustomer = JSON.parse(retCustomer);
 
-console.log(jsonCustomer);
+// console.log(jsonCustomer);
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+const customerList =[];
+
+function btnAddCustomerOnAction(){
+    let customer = {
+        id:document.getElementById("txtCustomerId").value,
+        name:document.getElementById("txtCustomerName").value,
+        age:document.getElementById("txtCustomerAge").value,
+        address:document.getElementById("txtCustomerAddress").value
+    }
+    console.log(customer);
+
+    customerList.push(customer);
+
+    console.log(customerList);
+    
+    localStorage.setItem("customerList",JSON.stringify(customerList));
+
+    // alert("add customer")
+}
+
+function btnSearchByIdOnAction(){
+    alert("search customer")
+}
+
+function btnDeleteByIdOnAction(){
+    alert("delete customer");
+}
+
+function btnUpdateByIdOnAction(){
+    alert("update customer");
+}
+
+function btnClearStorageOnAction(){
+    localStorage.clear();
+}
+
+function btnLoadTableOnAction(){
+    alert("load table");
+}
