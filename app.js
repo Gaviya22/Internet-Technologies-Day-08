045a45c48545a5690a72d0a35277ea4db551fb9a
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-25 09:06:53 +0530

    Add commented localStorage operations for future reference

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hii");
+
+// localStorage.setItem("name", "kamal");
+// localStorage.setItem("name1", "saman");
+
+// localStorage.removeItem("name1");
+
+// let custName = localStorage.getItem("name");
+// console.log(custName);
+
+
